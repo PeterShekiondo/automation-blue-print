dc5394cfe3fc48dd981f378936e7782e360e0b00
Peter David <peter@codeblock.co.tz>
2022-12-11 14:23:14 +0300

    define sample mutation

--- lib/api/gql-request/interceptors/adapters/native-request.adapter.ts
+++
@@ -1,6 +0,0 @@
-import {GQLContext} from '../context'
-
-export function NativeRequestAdapter (ctx: GQLContext){
-        
-    return Array.of(ctx.req.type, ctx.req.variables, ctx.req.headers)
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import { serverUrl } from "../env";
+
+const client = new ApolloClient({
+  uri: serverUrl,
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
